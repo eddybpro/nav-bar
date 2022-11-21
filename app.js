883bbb7efb8d762319a2821bb5fe519cb12d8d5a
eddybpro <abdelilahdb154@gmail.com>
2022-11-21 14:20:49 +0100

    nabar

---
+++ app.js
@@ -0,0 +1,18 @@
+const menu = document.getElementById('menu');
+const list = document.querySelector('.list');
+const closeBtn = document.querySelector('.fa-times');
+const li_el = list.querySelectorAll('li');
+
+
+menu.addEventListener('click',()=>{
+    list.classList.add('active')
+    menu.classList.toggle('fa-times')
+})
+
+
+li_el.forEach(item=>{
+    item.addEventListener('click',()=>{
+        list.classList.remove('active')
+        menu.classList.toggle('fa-times')
+    })
+})
